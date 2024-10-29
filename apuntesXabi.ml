4cf08017f36bc2c5914b0ab40f7be3777e97e249
Xabi <114141049+Xabiguitian@users.noreply.github.com>
2024-10-29 10:32:34 +0100

    29/10/24

--- apuntesXabi.ml
+++ apuntesXabi.ml
@@ -606,3 +606,76 @@ val for_all : ('a -> bool) -> 'a list -> bool = <fun>
 - : bool = true
 # for_all (fun n-> n mod 2 = 0) [2;3;8];;
 - : bool = false
+
+(*-----------------------------------------------------------------*)
+
+# let rec ordenada = function
+      [] | _::[] -> true
+   | h1::h2::t -> h1 <= h2 && ordenada(h2::t);;
+val ordenada : 'a list -> bool = <fun>
+# ordenada [4;1;3];;
+- : bool = false
+# ordenada [1;2;3];;
+- : bool = true
+# let rec insert x = function
+      [] -> [x]
+    | h::t -> if x < h then x::h::t
+          else h::insert x t;;
+val insert : 'a -> 'a list -> 'a list = <fun>
+# insert 2;;
+- : int list -> int list = <fun>
+# insert [1;2;3] 4;;
+Error: This expression has type int but an expression was expected of type
+         int list list
+# insert 4 [1;2;3];;
+- : int list = [1; 2; 3; 4]
+# insert 1 [1;2;3];;
+- : int list = [1; 1; 2; 3]
+# let rec i_sort = function
+      [] -> []
+    | h::t -> insert h (i_sort t);;
+val i_sort : 'a list -> 'a list = <fun>
+# i_sort 4 [1;2;3;4;5];;
+Error: The function i_sort has type 'a list -> 'a list
+       It is applied to too many arguments
+Line 1, characters 9-20:   This extra argument is not expected.
+# i_sort [1;0;-1;2;9];;
+- : int list = [-1; 0; 1; 2; 9]
+# i_sort (List.rev (List.init 20_000 succ))
+ (*Aplicación is_sort para su peor caso*);;
+- : int list =
+[1; 2; 3; 4; 5; 6; 7; 8; 9; 10; 11; 12; 13; 14; 15; 16; 17; 18; 19; 20; 21;
+ 22; 23; 24; 25; 26; 27; 28; 29; 30; 31; 32; 33; 34; 35; 36; 37; 38; 39; 40;
+ 41; 42; 43; 44; 45; 46; 47; 48; 49; 50; 51; 52; 53; 54; 55; 56; 57; 58; 59;
+ 60; 61; 62; 63; 64; 65; 66; 67; 68; 69; 70; 71; 72; 73; 74; 75; 76; 77; 78;
+ 79; 80; 81; 82; 83; 84; 85; 86; 87; 88; 89; 90; 91; 92; 93; 94; 95; 96; 97;
+ 98; 99; 100; 101; 102; 103; 104; 105; 106; 107; 108; 109; 110; 111; 112;
+ 113; 114; 115; 116; 117; 118; 119; 120; 121; 122; 123; 124; 125; 126; 127;
+ 128; 129; 130; 131; 132; 133; 134; 135; 136; 137; 138; 139; 140; 141; 142;
+ 143; 144; 145; 146; 147; 148; 149; 150; 151; 152; 153; 154; 155; 156; 157;
+ 158; 159; 160; 161; 162; 163; 164; 165; 166; 167; 168; 169; 170; 171; 172;
+ 173; 174; 175; 176; 177; 178; 179; 180; 181; 182; 183; 184; 185; 186; 187;
+ 188; 189; 190; 191; 192; 193; 194; 195; 196; 197; 198; 199; 200; 201; 202;
+ 203; 204; 205; 206; 207; 208; 209; 210; 211; 212; 213; 214; 215; 216; 217;
+ 218; 219; 220; 221; 222; 223; 224; 225; 226; 227; 228; 229; 230; 231; 232;
+ 233; 234; 235; 236; 237; 238; 239; 240; 241; 242; 243; 244; 245; 246; 247;
+ 248; 249; 250; 251; 252; 253; 254; 255; 256; 257; 258; 259; 260; 261; 262;
+ 263; 264; 265; 266; 267; 268; 269; 270; 271; 272; 273; 274; 275; 276; 277;
+ 278; 279; 280; 281; 282; 283; 284; 285; 286; 287; 288; 289; 290; 291; 292;
+ 293; 294; 295; 296; 297; 298; 299; ...]
+# let crono f c = let t = Sys.time () in let _ = f x in Sys.time () -. t;;
+Error: Unbound value x
+#let crono f x = let t = Sys.time () in let _ = f x in Sys.time () -. t;;
+val crono : ('a -> 'b) -> 'a -> float = <fun>
+# crono i_sort (List.rev (List.init 10_000 succ));;
+- : float = 1.23573299999999975
+# crono i_sort (List.rev (List.init 40_000 succ));;
+- : float = 31.3513329999999968
+# 5. ** 4.;;
+- : float = 625.
+# crono i_sort (List.init 20_000 (fun _ -> Random.int 200_000));;
+- : float = 2.73980300000000199
+# crono i_sort (List.init 180_000 succ);;
+- : float = 0.0381529999999941083
+# crono i_sort (List.init 300_000 succ);;
+- : float = 0.0571350000000023783
